cd5ec4c7d49b5fb3f868dd57b30cb189ee1cb7ab
PipahCookies <rbiatul21@gmail.com>
2025-12-07 23:18:59 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'brain-arcade-v6-offline';
+const ASSETS = [
+  './infinite.html',
+  './manifest.json',
+  'https://fonts.googleapis.com/css2?family=Orbitron:wght@500;700;900&family=Poppins:wght@400;600&display=swap'
+];
+
+// 1. Install Service Worker & Cache Files
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// 2. Serve Files from Cache (Offline Support)
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
